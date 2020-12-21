7f1366f1206b027a0e4a946529fe402e58496039
Phillip <phillip.mittmann@gmail.com>
2020-12-20 22:32:21 -0300

    Correcao rota de busca de livros disponiveis

--- front-end/src/common/Urls.js
+++ front-end/src/common/Urls.js
@@ -1,5 +1,5 @@
 export const URL_BASE = '192.168.0.10:8080';
 export const DOAR_LIVRO = `${URL_BASE}/doarLivro`;
-export const LIVROS = `${URL_BASE}/livros`;
+export const LIVROS = `${URL_BASE}/livrosDisponiveis`;
 export const RETIRAR_LIVRO = `${URL_BASE}/retirarLivro`;
 export const DEVOLVER_LIVRO = `${URL_BASE}/devolverLivro`;
